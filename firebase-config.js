7c93b32f32b2e7ef77085b6f7da8bb6dd2db20b2
Alejandro Basilio <alejandrobasilio@example.com>
2026-05-30 13:42:56 -0600

    Add firebase-config.js - fixes 404 error

---
+++ firebase-config.js
@@ -0,0 +1,4 @@
+// Firebase configuration - set to null to use local authentication (IndexedDB)
+// If you want to enable Firebase, add your Firebase project credentials here
+
+window.FIREBASE_CONFIG = null;
